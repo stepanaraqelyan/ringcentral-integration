2f9d14748d7e43f517412350ce888674c2d3fffc
Stepan Araqelyan <stepanaraqelyan1990@gmail.com>
2017-11-27 12:55:13 +0400

    cnf

--- config.php
+++ config.php
@@ -7,11 +7,10 @@ return [
 		'user' => 'root',
 		'pass' => 'localhost'
 	],
-        'rc' => [
-                'api_url' => 'https://platform.ringcentral.com/restapi/v1.0',
-                'access_token' => 'SUFEMDFQMDhQQVMwMHxBQUNPNEQ0RnhJZm9sNlBFM1g0$
-        ],
-
+	'rc' => [
+		'api_url' => 'https://platform.ringcentral.com/restapi/v1.0',
+		'access_token' => 'SUFEMDFQMDhQQVMwMHxBQUNPNEQ0RnhJZm9sNlBFM1g0TlNYZTRucnJMTGhQYVB2azAtMldxLTZMRFNQM252NmJob0N1c3FWM0s2cjJ4TGlWNF9QWWRPMEVYNEZjSG5zR19MNFVGb3d6bXZxSlB4U3VDSFhSRmgyQWI5QjN1RlJpYmxZMXlJTFZCUlBlNFBrQURTN3hoNndGa3hHRjUteDdVRjd0c1NQaVE3Q0ZGMl9PTEgtTERiWUtYMkdFRFRrVmpNOGprZWUtbWVWOW45Wmd8TmU4RjVBfFoxMEFXX21ZZVNTVzMtSlpWT01sN1E'
+	],
 	'dir' => __DIR__,
 	'views' => __DIR__ . '/views'
 ];
