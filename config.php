3460f0fec03d3036a9fbb677d519ed46c3a424c7
Stepan Araqelyan <stepanaraqelyan1990@gmail.com>
2017-11-27 12:50:13 +0400

    cnf

--- config.php
+++ config.php
@@ -3,10 +3,15 @@
 return [
 	'db' => [
 		'host' => 'localhost',
-		'name' => 'operator',
+		'name' => 'operators',
 		'user' => 'root',
-		'pass' => ''
+		'pass' => 'localhost'
 	],
+        'rc' => [
+                'api_url' => 'https://platform.ringcentral.com/restapi/v1.0',
+                'access_token' => 'SUFEMDFQMDhQQVMwMHxBQUNPNEQ0RnhJZm9sNlBFM1g0$
+        ],
+
 	'dir' => __DIR__,
 	'views' => __DIR__ . '/views'
 ];
